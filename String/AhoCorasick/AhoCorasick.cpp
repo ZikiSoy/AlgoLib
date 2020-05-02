ef2a16f9f55c9b9c2ad143f0ba4001a2ae6b412f
Ziki <zikisoy@gmail.com>
2020-05-02 21:06:27 +0100

    Fix AC template

--- String/AhoCorasick/AhoCorasick.cpp
+++ String/AhoCorasick/AhoCorasick.cpp
@@ -1,67 +1,75 @@
-#include <queue>
-#include <algorithm>
-
-using namespace std;
-
 int idx(char c) { 
-    if (c == 'A') return 0;
-    if (c == 'T') return 1;
-    if (c == 'C') return 2;
-    if (c == 'G') return 3;
+    return c - 'a';
 }
 
 struct AhoCorasick {
-  static const int maxnode = 200005;
+  static const int maxnode = 26 * 100005;
   static const int sigma_size = 26;
-  
-    int ch[maxnode][sigma_size]; //child
-    int f[maxnode]; //fail 
-    int val[maxnode]; //value
-    int match[maxnode]; //max match
-    int len[maxnode]; //length of a node
-    int sz; //size
-    void reset() { sz = 1; memset(ch[0], 0, sizeof(ch[0])); }
 
-    void insert(char *s, int v) {
-        int u = 0, n = strlen(s);
-        for (int i = 0; i < n; i++) {
-            int c = idx(s[i]);
-            if (!ch[u][c]) {
-                memset(ch[sz], 0, sizeof(ch[sz]));
-                match[sz] = val[sz] = 0;
-                len[sz] = len[u] + 1;
-                ch[u][c] = sz++;
-            }
-            u = ch[u][c];
-        }
+  int ch[maxnode][sigma_size]; //child
+  int f[maxnode]; //fail 
+  int64 val[maxnode]; //value
+  int match[maxnode]; //max match
+  int len[maxnode]; //length of a node
+  int sz; //size
+  void reset() { sz = 1; memset(ch[0], 0, sizeof(ch[0])); }
 
-        match[u] = n;
-        val[u] = v;
-    }
+  void insert(const char *s, int v) {
+      int u = 0, n = strlen(s);
+      for (int i = 0; i < n; i++) {
+          int c = idx(s[i]);
+          if (!ch[u][c]) {
+              memset(ch[sz], 0, sizeof(ch[sz]));
+              match[sz] = val[sz] = 0;
+              len[sz] = len[u] + 1;
+              dout << "insert " << s[i] << ' ' <<sz << endl;
+              ch[u][c] = sz++;
+          }
+          u = ch[u][c];
+      }
 
-    int getFail() {
-        queue<int> q;
-        f[0] = 0;
+      match[u] = n;
+      val[u] = v;
+      dout << "value " << u << ' ' << v << endl;
+  }
 
-        for (int c = 0; c < sigma_size; c++) {
-            int u = ch[0][c];
-            if (u) { f[u] = 0; q.push(u); }
-        }
+  void getFail() {
+      queue<int> q;
+      f[0] = 0;
 
-        while (!q.empty()) {
-            int r = q.front(); q.pop();
-            for (int c = 0; c < sigma_size; c++) {
-                int &v = ch[r][c];
-                if (!v) { 
-                    v = ch[f[r]][c]; 
-                } else {
-                    q.push(v);
-                    f[v] = ch[f[r]][c];
-                    val[v] = val[v]|val[f[v]];
-                    match[v] = max(match[v], match[f[v]]);
-                }
-            }
-        }
-    }
+      for (int c = 0; c < sigma_size; c++) {
+          int u = ch[0][c];
+          if (u) { f[u] = 0; q.push(u); }
+      }
+
+      while (!q.empty()) {
+          int r = q.front(); q.pop();
+          for (int c = 0; c < sigma_size; c++) {
+              int &v = ch[r][c];
+              if (!v) { 
+                  v = ch[f[r]][c]; 
+              } else {
+                  q.push(v);
+                  f[v] = ch[f[r]][c];
+                  // How to combine value with same suffix.
+                  // val[v] = val[v]|val[f[v]];
+                  val[v] = val[v] + val[f[v]];
+                  dout << v << ' ' << val[v] << endl;
+                  match[v] = max(match[v], match[f[v]]);
+              }
+          }
+      }
+  }
+  // Find string starting from node j.
+  int64 find(const char* T, int j = 0) {
+      int n = strlen(T);
+      int64 ret = 0;
+      for (int i = 0; i < n; i++) {
+          int c = idx(T[i]);
+          while (j && !ch[j][c]) j = f[j];
+          j = ch[j][c];
+          ret += val[j];
+      }
+      return ret;
+  }
 } ac;
-
